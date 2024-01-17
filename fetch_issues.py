70efea34af245a6f9ade811065e09d13461ca4a5
Carlos Brandt <carloshenriquebrandt@gmail.com>
2024-01-17 20:54:57 +0100

    Fix CSV reading (avoid 1st line)

--- fetch_issues.py
+++ fetch_issues.py
@@ -1,6 +1,6 @@
 import csv
 import requests
-
+from pprint import pprint
 
 def fetch_issues(repo, label='documentation'):
     """
@@ -17,6 +17,37 @@ def fetch_issues(repo, label='documentation'):
                 'url': issue['html_url']
             }
     return issues
+
+
+_CSV_ARGS = dict(
+    fieldnames = ['repo', 'number', 'title', 'url'],
+    quoting = csv.QUOTE_NONNUMERIC,
+)
+
+
+def read_issues(issues_file):
+    issues = {}
+    with open(issues_file) as file:
+        reader = csv.DictReader(file, **_CSV_ARGS)
+        # Escape the first/header line
+        next(reader)
+        for issue in reader:
+            repo = issue.pop('repo')
+            number = issue.pop('number')
+            key = f"{repo}:{number}"
+            issues[key] = issue
+    return issues
+
+
+def write_issues(issues, filename):
+    with open(filename, 'w') as file:
+        writer = csv.DictWriter(file, **_CSV_ARGS)
+        # Write header line (ie, fieldnames)
+        writer.writeheader()
+        for key,issue in issues.items():
+            repo, number = key.split(':')
+            issue.update({'repo':repo, 'number':number})
+            writer.writerow(issue)
 
 
 def read_list(filename):
@@ -38,41 +69,50 @@ def read_list(filename):
     return lines
 
 
-def write_table(all_issues, filename):
-    table = []
-    fieldnames = ['repo', 'number', 'title', 'url']
-    with open(filename, 'w') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames, quoting=csv.QUOTE_NONNUMERIC)
-        writer.writeheader()
-        for key,issue in all_issues.items():
-            repo, number = key.split(':')
-            issue.update({'repo':repo, 'number':number})
-            writer.writerow(issue)
-
 def main():
     """
     Write CSV file from 'documentation' issues from 'repos_source.txt' file
 
-    Github repositories listed in 'repos_source.txt' file are queried for issues
+    Github repositories listed in 'repos.list' file are queried for issues
     with label 'documentation' associated. Title, URL, and issue's number
-    are collected and written in 'jupyter_docs_issues.csv'.
+    are collected and written in 'issues.csv'.
     """
     # List of (Github) repositories to query
-    repos_file = 'repos_source.txt'
+    repos_file = 'repos.list'
     # Issues' label (applied to all repos)
     label = 'documentation'
     # Retrieved issues table
-    issues_file = 'jupyter_docs_issues.csv'
+    issues_file = 'issues.csv'
 
+    # Read list of repositories
     repos = read_list(repos_file)
+    print("Repos:", repos)
 
-    all_issues = {}
+    # Read current/pass list of issues
+    try:
+        current_issues = read_issues(issues_file)
+        print("Current issues:")
+        pprint(current_issues.keys())
+    except FileNotFoundError as err:
+        print("No previous issues to read.")
+        current_issues = None
+
+    issues = {}
     for repo in repos:
-        all_issues.update(
+        issues.update(
             fetch_issues(repo, label=label)
             )
+    print("Queried issues:")
+    pprint(issues.keys())
 
-    write_table(all_issues, issues_file)
+    if current_issues:
+        print("Diff:", set(issues).symmetric_difference(current_issues))
+
+    all_issues = current_issues.copy() if current_issues else {}
+    all_issues.update(issues)
+
+    write_issues(all_issues, issues_file)
+
 
 if __name__ == "__main__":
     main()
